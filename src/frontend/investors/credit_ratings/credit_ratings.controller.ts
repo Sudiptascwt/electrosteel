cebc09ea2b2cd122a1db5d62c7955c689cd1af7f
moumitascwt <dev2.scwt@gmail.com>
2025-10-29 12:17:04 +0530

    download files

--- src/frontend/investors/credit_ratings/credit_ratings.controller.ts
+++ src/frontend/investors/credit_ratings/credit_ratings.controller.ts
@@ -1,10 +1,16 @@
 import {
   Controller,
   Get,
+  NotFoundException,
+  Param,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { FrontendCreditRatingsService } from './credit_ratings.service';
 import { ApiKeyGuard } from 'src/common/api-key.guard';
+import { Response } from 'express'; 
+import * as path from 'path';       
+import * as fs from 'fs';     
 
 @UseGuards(ApiKeyGuard)
 @Controller()
@@ -15,5 +21,23 @@ export class FrontendCreditRatingsController {
     @Get('frontend/investor/credit-ratings')
     async getCreditRatingsData() {
         return this.CreditRatingsService.getCreditRatingsData();
+    }
+
+    @Get('download/:filename')
+    async downloadFile(
+      @Param('filename') filename: string,
+      @Res() res: Response,
+    ) {
+      const filePath = path.join(process.cwd(), 'uploads', filename);
+
+      if (!fs.existsSync(filePath)) {
+        throw new NotFoundException(`File ${filename} not found`);
+      }
+
+      res.download(filePath, filename, (err) => {
+        if (err) {
+          throw new NotFoundException('Error while downloading file');
+        }
+      });
     }
 }
